eefa51e568fab258878ea18bfdb30f13eadfa86a
dtcurrie <deltron0>
2016-06-10 10:44:58 -0400

    - styling for iframe

---
+++ widget/embed.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+    document.getElementById("yahooweather")
+        .innerHTML = '<object type="type/html" data="https://rawgit.com/DTCurrie/yahooweather/master/widget/index.html" ></object>';
+};
