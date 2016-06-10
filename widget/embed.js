f8be880d97cb5877af1377fc068b42612af7af9e
dtcurrie <deltron0>
2016-06-10 10:54:39 -0400

    - styling for embed

--- widget/embed.js
+++ widget/embed.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+    console.log("loaded");
     document.getElementById("yahooweather")
         .innerHTML = '<object type="type/html" data="https://rawgit.com/DTCurrie/yahooweather/master/widget/index.html" ></object>';
 };
