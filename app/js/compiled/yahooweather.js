e7143d79fd76f1d0ee1bfa7456b75e7913db4adb
dtcurrie <deltron0>
2016-06-10 10:24:16 -0400

    - api call to https

--- app/js/compiled/yahooweather.js
+++ app/js/compiled/yahooweather.js
@@ -1,7 +1,7 @@
 var weather = angular.module("weather", [])
     .controller('MainController', ['$http', '$scope',
         function ($http, $scope) {
-            $http.get("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22mclean%2Cva%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+            $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22mclean%2Cva%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
                 .then(function (response) {
                     $scope.weather = response.data.query.results.channel;
                     $scope.weather.img = "";
